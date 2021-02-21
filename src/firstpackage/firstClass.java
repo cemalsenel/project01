c93fb09ce884597bc3794fda0e0a14412a953c24
Cemal Senel <senelcemal61@gmail.com>
2021-02-21 09:54:48 +0100

    second commit from local master

--- src/firstpackage/firstClass.java
+++ src/firstpackage/firstClass.java
@@ -5,6 +5,7 @@ public class firstClass {
 	public static void main(String[] args) {
 		
 		System.out.println("First commit from local master");
+		System.out.println("Second line of code i am typing");
 
 	}
 
